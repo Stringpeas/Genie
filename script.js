aee65eb93c5f329b860038671a0947dd41bbb1d3
Stringpeas <60332702+Stringpeas@users.noreply.github.com>
2022-04-29 03:38:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,26 +24,6 @@ strings.push(
 
 )
 
-strings.push(
-
-    "From pebbles and sticks I began the<br>\
-    creation of my city<br>\
-    <br>\
-    A flourishing metropolis<br>\
-    that ran on the fuel of fear<br>\
-    <br>\
-    Stores built from denial<br>\
-    Skyscrapers molded by sorrow<br>\
-    Roads paved with blame<br>\
-    <br>\
-    It's crazy to think<br>\
-    This city I thought was impenetrable<br>\
-    <br>\
-    Collapsed into rubble<br>\
-    The second your stubble<br>\
-    touched my lips"
-
-)
 
 strings.push(
 
