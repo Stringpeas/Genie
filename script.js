66b4f95b13b71423544bf9d227ef54d2173284d5
Stringpeas <60332702+Stringpeas@users.noreply.github.com>
2023-08-14 18:45:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -78,6 +78,66 @@ strings.push(
 
 )
 
+strings.push(
+
+    "My two brown boots, wrapped in green on their trek<br>\
+    Where they come face to face with a red rose<br>\
+    Who smells as sweet with petals made of mountain<br>\
+    <br>\
+    The promised pretty pastures painted on maps filled my eyes with sparkle<br>\
+    Winding withered paths that lead down rural roads<br>\
+    Where the days came hearty with an apple in its mouth<br>\
+    <br>\
+    Half-way through my journey everything fell<br>\
+    Sand slipping through my fingers<br>\
+    Dust twirling me into a choke<br>\
+    <br>\
+    When I found my feet I looked back at the map<br>\
+    Magnetized by the X marked the spot of my sweet rose<br>\
+    A decision that took much more than the tightening of laces<br>\
+    <br>\
+    To my surprise, lonsesome on the X, was a rose<br>\
+    Now with a deeper, richer purple colour<br>\
+    Grown and stained by tears<br>\
+    <br>\
+    Tears that are now in my own smiled eyes<br>\
+    The storm had changed us both<br>\
+    And that is beautiful<br>\
+    <br>\
+    I’d miss the aftertaste of your love that lingered in my heart,<br>\
+    But shocked I realized I could never find the rose I once knew, twice,<br>\
+    Not even in the same rose."
+
+)
+
+
+strings.push{
+    "i saw a pretty leaf today<br>\
+
+    that will defy laws of matter tomorrow when it vanishes in midair<br>\
+    
+    there are things i’ve done today that i struggle to recall, things i've seen but haven't watched, things i've heard but haven't listened<br>\
+
+    i continue to roll the pebble that i found not too long ago in my shoe, who will soon disappear once i exhaust everything she has to offer, simple dull periodic jabs to my big toe, but nothing he hasn’t dealt with before<br>\
+
+    today i took the bus to school, and i think, because im always thinking around the people i trust the least, and i try to recall the temporary, the fleet that will flee<br>\
+
+    the puddle my left foot fell into walking to the bus stop, the bus that came a minute too early driving past my lustful eyes moving left to right, the awful smell from the garbage can that should really be cleaned out more often sitting to the left of the bus stop who I'm already pretty upset at<br>\
+
+    meanwhile, the american robin effortlessly sitting on a tree who could probably win a red ribbon in a basket weaving competition, the delicious shade from the side of an old pharmacy created at my usual waiting spot, the wave of an innocent child greeting me as i sit down on the bus and indulge in a smile<br>\
+
+    today joins the long list of yesterdays that thankfully never seems to end, depending on when you ask me of course, but there really isn’t any time for that right now because the days i cherish seem to go by twice as fast<br>\
+
+    tomorrow carries me by the nape, a painless paralyzing pickup into the pit of opportunity. A place where we ought to forget and find comfort that things go. What I’m feeling now will not last forever, which draws pleasure on those painful days<br>\
+
+    lets freeze this insignificant moment<br>\
+
+    i pick up my journal<br>\
+
+    i cant remember if the leaf was red or yellow."
+}
+
+
 function next() {
 
     if(strings[currentArrayValue] != null){
